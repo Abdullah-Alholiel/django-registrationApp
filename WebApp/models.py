b6ac68ab556c8e571d76f453c386be18492bc7dc
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-07-13 21:42:39 +0000

    Models,Templates and navigation is initallized

--- WebApp/models.py
+++ WebApp/models.py
@@ -1,1 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from users.models import Student
+
+class Module(models.Model):
+    # Fields for Module details
+    name = models.CharField(max_length=255)  # The name of the module
+    code = models.CharField(max_length=255)  # The unique code of the module
+    credit = models.IntegerField()  # The credit points for the module
+    category = models.CharField(max_length=255)  # The category of the module
+    description = models.TextField()  # The detailed description of the module
+
+    # Boolean field indicating if the module is available for registration
+    availability = models.BooleanField(default=False)
+
+    # Boolean field indicating if the module is active or not
+    active = models.BooleanField(default=True)
+
+    # Many-to-many relationship with Group model
+    # This allows multiple groups (courses) to have the same module
+    groups = models.ManyToManyField(Group, related_name='modules')
+
+    class Meta:
+        # Ensuring that the combination of name and code is unique
+        unique_together = ('name', 'code',)
+
+class Registration(models.Model):
+    # Foreign keys to Student and Module models
+    # Each registration is associated with one student and one module
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    # Automatic timestamp for when the registration was created
+    date_of_registration = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Ensuring that the combination of student and module is unique
+        # This prevents a student from registering to the same module multiple times
+        unique_together = ('student', 'module',)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,19 @@
+from django.db import models
+from django import forms
+from django.contrib.auth.models import User, Group
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    course = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='student')
+    date_of_birth = models.DateField()  # Date of birth of the student
+    address = models.CharField(max_length=255)  # Address of the student
+    city = models.CharField(max_length=255)  # City of the student
+    country = models.CharField(max_length=255)  # Country of the student
+    photo = models.ImageField(upload_to='student_photos/', blank=True, null=True)  # Student's photo
+
+    def __str__(self):
+        return self.user.first_name
+
+
 
