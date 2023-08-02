d70a24f660df3d6e822025cae18b8535a82ee3b7
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-08-02 15:26:02 +0100

    updated models in admin and foldering structure

--- users/models.py
+++ users/models.py
@@ -4,16 +4,20 @@ from django.contrib.auth.models import User, Group
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
-    course = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='student')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student', related_query_name='student')
+    course = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='student_course', related_query_name='student_course')
     date_of_birth = models.DateField()  # Date of birth of the student
     address = models.CharField(max_length=255)  # Address of the student
     city = models.CharField(max_length=255)  # City of the student
     country = models.CharField(max_length=255)  # Country of the student
-    photo = models.ImageField(upload_to='student_photos/', blank=True, null=True)  # Student's photo
-
+    picture = models.ImageField(default='default.jpeg', upload_to='student_photos/')  # Student's photo
+    class Meta:
+        ordering =['user']
+        # verbos_name ='Items'
+        
     def __str__(self):
-        return self.user.first_name
+        return f'{self.user.username} Student'
+
 
 
 
